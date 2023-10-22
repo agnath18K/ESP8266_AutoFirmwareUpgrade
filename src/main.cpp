221b95ad3c12d6f3c5fd3c81d84b84813808368a
Arun Gopinath <agnath18K@gmail.com>
2023-10-22 18:59:26 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,177 +21,97 @@ Using Github as host Server.
   agnath18 :) 
 
 */
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
 #include <WiFiClientSecure.h>
 #include <CertStoreBearSSL.h>
-BearSSL::CertStore certStore;
 #include <time.h>
 
-#define WIFI_SSID "agnath18"
-#define WIFI_PASSWORD "123456789"
+BearSSL::CertStore certStore;
+#include "TrustRoot.h" 
 
-#define HOST_URL "raw.githubusercontent.com";
-#define VERS_URL "https://raw.githubusercontent.com/agnath18K/ESP8266_AutoFirmwareUpgrade/main/bin/version"
-#define Firm_URL "https://raw.githubusercontent.com/agnath18K/ESP8266_AutoFirmwareUpgrade/main/bin/firmware.bin"
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+#define HOST_URL "your-github-host-url" 
+#define VERS_URL "https://raw.githubusercontent.com/your-username/your-repo/main/bin/version"
+#define FIRM_URL "https://raw.githubusercontent.com/your-username/your-repo/main/bin/firmware.bin"
 
-double Firm_Ver = 1.00;
-const unsigned long Update_Interval = 60000;
+const char *host = HOST_URL;
 const int httpsPort = 443;
+const unsigned long UpdateInterval = 60000; 
 
-unsigned long counter = 0;
-double Lat_Ver;
-int Error_Count=1;
-const char* host = HOST_URL;
+WiFiClientSecure client;
 
-const char trustRoot[] PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs
-MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
-d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j
-ZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAwMFoXDTMxMTExMDAwMDAwMFowbDEL
-MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3
-LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFuY2Ug
-RVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm
-+9S75S0tMqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTW
-PNt0OKRKzE0lgvdKpVMSOO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEM
-xChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFB
-Ik5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQNAQTXKFx01p8VdteZOE3
-hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUeh10aUAsg
-EsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQF
-MAMBAf8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaA
-FLE+w2kD+L9HAdSYJhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3Nec
-nzyIZgYIVyHbIUf4KmeqvxgydkAQV8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6z
-eM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFpmyPInngiK3BD41VHMWEZ71jF
-hS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkKmNEVX58Svnw2
-Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe
-vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep
-+OkuE6N36B9K
------END CERTIFICATE-----
-)EOF";
-X509List cert(trustRoot);
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nStarting ESP8266 Auto Firmware Upgrade");
+  ConnectWiFi();
+  configTime(0, 0, "pool.ntp.org", "time.google.com");
+}
 
-void Error_Con();
-void Connect_WiFi();
-void setClock();
+void loop() {
+  unsigned long currentMillis = millis();
+  static unsigned long previousMillis = 0;
 
-void Error_Con() {
-  Serial.print("\nError Detected.\nError Count : ");
-  Serial.print(Error_Count);
-  Error_Count+=1;
-  if(Error_Count>3)  {
-    Serial.print("\nProceeding ESP Restart In 60 Seconds\n");
-    Error_Count=0;
-    delay(60000);
-    Serial.print("\n\nRestarting ESP\n\n");
-    ESP.restart(); }
-  else {
-    Serial.println("\nRestarting WiFi Connection");
-    Connect_WiFi(); } }
-
-void Connect_WiFi() {
- WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- Serial.print("\n\nConnecting to Wi-Fi");
- int try_count=0;
- while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
-    try_count+=1;
-    if(try_count>=100) {
-    Serial.println("\nConnection taking too long.");
-    Error_Con(); }
+  if (currentMillis - previousMillis >= UpdateInterval) {
+    previousMillis = currentMillis;
+    CheckAndUpdateFirmware();
   }
- Serial.println();
- Serial.print("Connected with IP: ");
- Serial.println(WiFi.localIP());
- Serial.println();
- setClock(); }
+}
 
-void setClock() {
-  int ntp_count=0;
-  configTime(0, 0, "pool.ntp.org", "time.google.com"); 
-  Serial.print(F("\nWaiting for NTP time sync: "));
-  time_t now = time(nullptr);
-  while (now < 8 * 3600 * 2) {
-    yield();
+void ConnectWiFi() {
+  Serial.println("\nConnecting to Wi-Fi");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(F("."));
-    now = time(nullptr);
-    ntp_count+=1;
-    if(ntp_count>=100) {
-    Serial.println("\nConnection taking too long.");
-    Error_Con(); } } }
-      
-void Firmware_Update() {
- 
- BearSSL::WiFiClientSecure client;
- client.setTrustAnchors(&cert);
- Serial.print("\nConnection to host : ");
-
- if (!client.connect(host, httpsPort)) {
-    Serial.println("Failed");
-    Error_Con();
-    return; }
-  Serial.print("Success\n");
-
-  String url = VERS_URL;
-  Serial.print("\nCurrent Firmware Version : ");
-  Serial.print(Firm_Ver);
-
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      break; }
-     }
-  String line = client.readStringUntil('\n');
-  Lat_Ver = line.toDouble();
-  Serial.print("\nLatest Firmware Version   : ");
-  Serial.println(Lat_Ver);
-  if(Lat_Ver==404.00)
-  Serial.println("\nFailed to retrieve Version file.\nSkipping Firmware Check\n");
-  else {
-  if(Firm_Ver>=Lat_Ver)
-  Serial.println("\nDevice Running On Latest Firmware");
-  else
-  {
-  Serial.println("\nNew Firmware Found!");
-  Serial.println("\nStarting Upgrade\n");
-  ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW); 
-  t_httpUpdate_return ret = ESPhttpUpdate.update(client, Firm_URL);
-
-    switch (ret) {
-      case HTTP_UPDATE_FAILED:
-        Serial.printf("FIRMWARE_UPDATE_FAILED Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-        break;
-
-      case HTTP_UPDATE_NO_UPDATES:
-        Serial.println("FIRMWARE_UPDATE_NO_UPDATES");
-        break;
-
-      case HTTP_UPDATE_OK:
-        Serial.println("FIRMWARE_UPDATE_OK");
-        break; } } } }
-
-void setup()
-{
- Serial.begin(115200);
- Serial.print("\n");
- Connect_WiFi();
- setClock();
- }
-
- void loop()
- {
-  if(millis() - counter >= Update_Interval) {
-  Firmware_Update();
-  counter = millis(); }
+    Serial.print(".");
   }
+  Serial.println("\nConnected to Wi-Fi");
+}
+
+void CheckAndUpdateFirmware() {
+  Serial.println("Checking for firmware updates...");
+
+  if (client.connect(host, httpsPort)) {
+    Serial.println("Connected to host");
+    client.setTrustAnchors(&certStore);
+    HTTPClient http;
+    http.begin(client, VERS_URL);
+
+    int httpCode = http.GET();
+    if (httpCode == HTTP_CODE_OK) {
+      double latestVersion = http.getString().toDouble();
+      Serial.print("Current Firmware Version: ");
+      Serial.println(Firm_Ver);
+      Serial.print("Latest Firmware Version: ");
+      Serial.println(latestVersion);
+
+      if (latestVersion > Firm_Ver) {
+        Serial.println("New firmware available. Updating...");
+        PerformFirmwareUpdate();
+      } else {
+        Serial.println("Device is up-to-date.");
+      }
+    } else {
+      Serial.println("Failed to retrieve version file.");
+    }
+    http.end();
+  } else {
+    Serial.println("Connection to host failed.");
+  }
+}
+
+void PerformFirmwareUpdate() {
+  Serial.println("Starting firmware update...");
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client, FIRM_URL);
+
+  if (ret == HTTP_UPDATE_OK) {
+    Serial.println("Firmware updated successfully.");
+    ESP.restart();
+  } else {
+    Serial.print("Firmware update failed. Error code: ");
+    Serial.println(ret);
+  }
+}
